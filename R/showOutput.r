f40a9c5f5890de03f63deef7fc100ded7616495c
EllaTemprosa <ella@bsc.gwu.edu>
2016-07-13 13:02:23 -0400

    Next development version of comets package to run through the first step.  this version works using the following code, also included in vignette dir <- system.file("extdata", package="CometsAnalyticsPackage", mustWork=TRUE) csvfile <- file.path(dir, "cometsInput.xlsx") exmetabdata <- readCOMETSinput(csvfile) exmodeldata <- getModelData(exmetabdata,colvars="age") excorrdata  <- getCorr(exmodeldata,exmetabdata,"DPP") makeOutputCSV("corr",excorrdata,"DPP")

---
+++ R/showOutput.r
@@ -0,0 +1,23 @@
+#---------------------------------------------------------
+# Create output CSV file ---------------------------------
+#---------------------------------------------------------
+#' Create output CSV file
+#'
+#' @param file filename
+#' @param dataf dataframe
+#' @param cohort cohort name
+#'
+#'
+#' @return a CSV file with results named with cohort
+#'
+#' @examples
+#' dir <- system.file("extdata", package="CometsAnalyticsPackage", mustWork=TRUE)
+#' csvfile <- file.path(dir, "cometsInput.xlsx")
+#' modeldata <- getModelData(readCSV(csvfile))
+#' corrmatrix <-getCorr(modeldata)
+#' @export
+makeOutputCSV <- function (file,dataf,cohort=""){
+  fname <- gsub('.','_',tolower(cohort), fixed = TRUE)
+  fname <- paste(fname,file,Sys.Date(),'.csv',sep = '')
+  write.csv(dataf, fname,quote=TRUE)
+}
