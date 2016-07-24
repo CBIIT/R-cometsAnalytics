b189ce3d8e372cfb764f376219eb26cbe5b3bc03
EllaTemprosa <ella@bsc.gwu.edu>
2016-07-24 17:59:21 -0400

    changes package name to COMETS and added modelspec option to preparedata.R to allow parsing of interactive and batch models

--- R/prepareData.r
+++ R/prepareData.r
@@ -1,16 +1,16 @@
 
 
-
-
 #---------------------------------------------------------
-# Get Model data -----------------------------------------
+# Get Model data and model components --------------------
 #---------------------------------------------------------
 #' Get data ready for the model runs in interactive or batch mode as specified in the input
 #'
 #' @param readData list from readComets
-#' @param rowvars   Outcome variables (usually metabolites and rendered in rows default in All metabolites)
-#' @param colvars   Exposure variables (usually covariates rendered in columns)
-#' @param adjvars   Adjustment covariates
+#' @param modelspec How model is specified (Interactive or Batch)
+#' @param modbatch  if batch, chosen model specified by batch mode
+#' @param rowvars   if Interactive, outcome variables (usually metabolites and rendered in rows default in All metabolites)
+#' @param colvars   if Interactive, exposure variables (usually covariates rendered in columns)
+#' @param adjvars   If Interactive, adjustment covariates
 #'
 #' @return a list comprising:
 #'
@@ -24,34 +24,61 @@
 #'
 #' @examples
 #'
-#' modeldata <- getModelData(readData(),modbatch="Interactive",colvars="age")
+#' modeldata <- getModelData(readData(),modelspec="Batch",colvars="age")
 #'
 #' @export
 
 getModelData <-
   function(readData,
+           modelspec = "Batch",
+           modbatch  = "",
            rowvars   = "All metabolites",
            colvars   = "",
            adjvars   = NULL) {
 
+    # figure out the model specification based on type (Interactive or Batch)
+    if (modelspec == "Interactive") {
       # adjust the variable names
       # found all metabolites
-      if (!is.na(match("All metabolites", rowvars)))
+      if (!is.na(match("All metabolites",rowvars)))
         rcovs <-
-          unique(c(rowvars[rowvars != "All metabolites"], c(readData[[2]])))
+          unique(c(rowvars[rowvars != "All metabolites"],c(readData[[2]])))
       else
         rcovs <- rowvars
 
 
-      if (!is.na(match("All metabolites", colvars)))
+      if (!is.na(match("All metabolites",colvars)))
         ccovs <-
-          unique(c(colvars[colvars != "All metabolites"], c(readData[[2]])))
+          unique(c(colvars[colvars != "All metabolites"],c(readData[[2]])))
       else
         ccovs <- colvars
 
       acovs <- adjvars
+    }
+    else if (modelspec == "Batch") {
+      # here we need to get the covariates defined from the excel sheet
+      # step 1. get the chosen model first
 
-    # merge smetab
+
+      mods<-dplyr::filter(as.data.frame(readData[["mods"]]),model==modbatch)
+      if (length(mods)>0 & mods$outcomes=="All metabolites")
+        rcovs<-c(readData[[2]])
+      else
+        rcovs<-as.vector(strsplit(mods$outcomes," ")[[1]])
+
+      if (length(mods)>0 & mods$exposure=="All metabolites")
+        ccovs<-c(readData[[2]])
+      else
+        ccovs<-as.vector(strsplit(mods$exposure," ")[[1]])
+
+      if (!is.na(mods$adjustment))
+        acovs<-as.vector(strsplit(mods$adjustment," ")[[1]])
+      else acovs<-NULL
+
+    }
+
+
+    # Keep only needed variables for the data
     if (length(acovs) == 0) {
       gdta <-
         subset(as.data.frame(readData[[1]]), select = c(ccovs, rcovs))
@@ -76,3 +103,5 @@ getModelData <-
     )
 
   }
+
+
