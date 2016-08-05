1180b7712be85c73b63ef9ed5bbc2c787131ca3d
mathelab <ewy.mathe@osumc.edu>
2016-08-05 09:06:54 -0400

    The function makeOutputCSV() can now be used as an accessor to display the correlation output.
    An additional variable type_output needs to be input into the function and takes in "display" or "CSVfile" as an argument. Use "display" to output to the screen...

--- R/showOutput.r
+++ R/showOutput.r
@@ -3,10 +3,10 @@
 #---------------------------------------------------------
 #' Create output CSV file
 #'
-#' @param file filename
-#' @param dataf dataframe
+#' @param type_output "display" or "CSVfile"
+#' @param filename if type_output is "CSVfile", name of file
+#' @param dataf correlation output (from function getCorr()) data frame
 #' @param cohort cohort name
-#'
 #'
 #' @return the filename of the CSV file with results named with cohort
 #'
@@ -16,11 +16,20 @@
 #' exmetabdata <- readCOMETSinput(csvfile)
 #' modeldata <- getModelData(exmetabdata,colvars="age",modbatch="1.1 Unadjusted")
 #' corrmatrix <-getCorr(modeldata,exmetabdata,"DPP")
-#' makeOutputCSV("corr",corrmatrix,"DPP")
+#' makeOutputCSV(type_output="CSVfile",filename="corr",dataf=corrmatrix,cohort="DPP")
 #' @export
-makeOutputCSV <- function (file,dataf,cohort=""){
+
+
+makeOutputCSV <- function (type_output="display",filename,dataf,cohort=""){
   fname <- gsub('.','_',tolower(cohort), fixed = TRUE)
-  fname <- paste(fname,file,Sys.Date(),'.csv',sep = '')
-  write.csv(dataf, fname,quote=TRUE)
-  fname
+  fname <- paste(fname,filename,Sys.Date(),'.csv',sep = '')
+  if (type_output == "display") {
+	return(dataf)
+  }
+  else if (type_output == "CSVfile") {
+	write.csv(dataf, fname,quote=TRUE)
+  }
+  else {
+	stop("type_output variable needs to be 'CSVfile' or 'display'")
+  }
 }
