ec97d42980090f3687490b2f375620820f66c64a
EllaTemprosa <ella@bsc.gwu.edu>
2016-08-09 12:25:45 -0400

    changed name of outputCorrResults to outputCSVResults to make it more generic for future analyses (logistics, linear models, etc)

--- R/showOutput.r
+++ R/showOutput.r
@@ -4,7 +4,7 @@
 #' Create output CSV file
 #'
 #' @param filename if type_output is "CSVfile", name of file
-#' @param dataf correlation output (from function getCorr()) 
+#' @param dataf correlation output (from function getCorr())
 #' @param cohort cohort name
 #'
 #' @return the filename of the CSV file with results named with cohort
@@ -15,11 +15,12 @@
 #' exmetabdata <- readCOMETSinput(csvfile)
 #' modeldata <- getModelData(exmetabdata,colvars="age",modbatch="1.1 Unadjusted")
 #' corrmatrix <-getCorr(modeldata,exmetabdata,"DPP")
-#' OutputCorrResults(filename="corr",dataf=corrmatrix,cohort="DPP")
+#' OutputCSVResults(filename="corr",dataf=corrmatrix,cohort="DPP") #get correlation results
+#' OutputCSVResults(filename="harmonization",dataf=exmetabdata$metab,cohort="DPP") #get harmonization results
 #' @export
 
 
-OutputCorrResults <- function (filename,dataf,cohort=""){
+OutputCSVResults <- function (filename,dataf,cohort=""){
   dataf=as.data.frame.list(dataf)
   fname <- gsub('.','_',tolower(cohort), fixed = TRUE)
   fname <- paste(fname,filename,Sys.Date(),'.csv',sep = '')
