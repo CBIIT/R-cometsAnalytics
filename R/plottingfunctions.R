c10a0fcfdc797c26741cd66e52bd322d52ba485b
mathelab <ewy.mathe@osumc.edu>
2016-09-19 15:36:38 -0400

    Changed names of files and moved plotting functions to enhancer code readability.

--- R/plottingfunctions.R
+++ R/plottingfunctions.R
@@ -71,4 +71,140 @@ plotMinvalues <- function(cometsdata,
    return(p)
 }
 
+#---------------------------------------------------------
+# showCorr
+#---------------------------------------------------------
+#' Function that returns top N lines of the getCorr() output
+#' @param corr COMETScorr class (S3) from getCorr() output
+#' @param nlines number of lines to return (default 50)
+#' @return first 50 lines of output
+#' @examples
+#' dir <- system.file("extdata", package="COMETS", mustWork=TRUE)
+#' csvfile <- file.path(dir, "cometsInput.xlsx")
+#' exmetabdata <- readCOMETSinput(csvfile)
+#' modeldata <- getModelData(exmetabdata,modbatch="1.1 Unadjusted")
+#' corrmatrix <-getCorr(modeldata,exmetabdata,"DPP")
+#' showCorr(corrmatrix)
+#' @export
+showCorr <- function(corr, nlines=50) {
+        return(utils::head(as.data.frame(corr),nlines))
+}
 
+#---------------------------------------------------------
+#' Show interactive heatmap using plot_ly
+#'
+#' @param ccorrmat correlation matrix (output of getCorr())
+#' @param rowsortby How row labels are sorted
+#' @param plothgt Plot height default 700
+#' @param plotwid Plot width default 800
+#' @param colscale colorscale, can be custom or named ("Hots","Greens","Blues","Greys","Purples") see \url{https://plot.ly/ipython-notebooks/color-scales/}
+#'
+#' @return a heatmap with outcomes as rows and exposures in columns.
+#'
+#' @references For colorscale reference: \url{https://plot.ly/ipython-notebooks/color-scales/}
+#'
+#' @examples
+#' dir <- system.file("extdata", package="COMETS", mustWork=TRUE)
+#' csvfile <- file.path(dir, "cometsInput.xlsx")
+#' exmetabdata <- readCOMETSinput(csvfile)
+#' modeldata <- getModelData(exmetabdata,modbatch="1.1 Unadjusted")
+#' corrmatrix <-getCorr(modeldata,exmetabdata,"DPP")
+#' showHeatmap(corrmatrix)
+#' @export
+
+showHeatmap <- function (ccorrmat,
+       rowsortby = "corr",
+       plothgt=700,
+       plotwid=800,
+       colscale="RdYlBu") {
+
+  exmetabdata=corr=exposure=metabolite_name=c()
+
+  # order the rows according to sort by
+  if (rowsortby == "metasc") {exmodeldata <- COMETS::getModelData(exmetabdata,modbatch="1.1 Unadjusted")
+
+    ccorrmat$metabolite_name <- suppressWarnings(
+      factor(ccorrmat$metabolite_name, levels =
+               ccorrmat$metabolite_name[rev(order(unlist(ccorrmat["metabolite_name"])))]))
+  } else {
+    ccorrmat$metabolite_name <- suppressWarnings(
+      factor(ccorrmat$metabolite_name, levels = ccorrmat$metabolite_name[order(unlist(ccorrmat[rowsortby]))]))
+  }
+
+  # stack the correlations together
+  ccorrmat <- ccorrmat[order(ccorrmat$metabolite_name),]
+  # Number of columns identified by suffix of .n
+
+  # plotly will not plot if there is only one row (so quick fix is to duplicate data)
+  if(nrow(ccorrmat)==1) {
+        ccorrmat=rbind(ccorrmat,ccorrmat)
+  }
+
+  ccorrmat %>%
+  plotly::plot_ly(z = signif(corr),x = exposure, y = metabolite_name,
+          type = "heatmap",
+          colorscale=colscale,
+          colorbar = list(title = "Correlation")) %>%
+  plotly::layout(height=plothgt,
+         width=plotwid,
+         margin = list(l = 200),
+         title = " ",      # layout's title: /r/reference/#layout-title
+         xaxis = list(           # layout's xaxis is a named list.
+           title = " ",       # xaxis's title: /r/reference/#layout-xaxis-title
+           showgrid = F,          # xaxis's showgrid: /r/reference/#layout-xaxis
+           ticks=""
+         ),
+         yaxis = list(           # layout's yaxis is a named list.
+           title = " ",        # yaxis's title: /r/reference/#layout-yaxis-title,
+           ticks=""
+         )
+         ,
+         legend = list(           # layout's yaxis is a named list.
+           title = "Correlation"        # yaxis's title: /r/reference/#layout-yaxis-title
+         )
+         ,
+         autosize = TRUE)
+}
+
+#---------------------------------------------------------
+# showHClust -----------------------------------------
+#---------------------------------------------------------
+#' Show interactive heatmap using d3heatmap with hierarchical clustering
+#'
+#' @description
+#' This function outputs a heatmap with hierarchical clustering.  It thus requires you to have at least 2 outcome and 2 exposure variables in your models.
+#'
+#' @param ccorrmat correlation matrix
+#' @param clust Show hierarchical clustering
+#' @param colscale colorscale, can be custom or named ("Hots","Greens","Blues","Greys","Purples") see \url{https://plot.ly/ipython-notebooks/color-scales/}
+#'
+#' @return a heatmap with outcomes as rows and exposures in columns.
+#'
+#' @references For colorscale reference: \url{https://plot.ly/ipython-notebooks/color-scales/}
+#'
+#' @examples
+#' dir <- system.file("extdata", package="COMETS", mustWork=TRUE)
+#' csvfile <- file.path(dir, "cometsInput.xlsx")
+#' exmetabdata <- readCOMETSinput(csvfile)
+#' modeldata <- getModelData(exmetabdata, modelspec="Interactive",colvars=c("age","bmi"))
+#' corrmatrix <-getCorr(modeldata,exmetabdata,"DPP")
+#' showHClust(corrmatrix)
+#' @export
+showHClust <- function (ccorrmat,
+                        clust = TRUE,
+                        colscale = "RdYlBu") {
+ metabolite_name=exposure=corr=c()
+  excorr <-
+    ccorrmat %>% dplyr::select(metabolite_name, exposure, corr) %>% tidyr::spread(exposure, corr)
+  rownames(excorr) <- excorr[, 1]
+
+  ncols <- ncol(excorr)
+  if(ncols <= 2)
+        stop("Cannot run heatmap because there is only one exposure variable")
+  d3heatmap::d3heatmap(excorr[, 2:ncols],
+            colors = colscale,
+            dendrogram = if (clust)
+              "both"
+            else
+              "none")
+}
