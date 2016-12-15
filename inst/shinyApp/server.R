8423eda5f6b7ce310c1fcc8ec418283c6842fa1e
mathelab <ewy.mathe@osumc.edu>
2016-12-15 17:06:02 -0500

    Fixed shiny app to take in a template (Basic or Age).

--- inst/shinyApp/server.R
+++ inst/shinyApp/server.R
@@ -26,44 +26,64 @@ shinyServer(function(input, output,session) {
        return(list(metabfile=metabfile,subjfile=subjfile,abundancesfile=abundfile))
 })
 
+  getIDChoices <- reactive({
+    mychoices=c(colnames(read.csv(req(as.character(parseFilePaths(rootVolumes,input$subjfile)$datapath)))),
+       colnames(read.csv(req(as.character(parseFilePaths(rootVolumes,input$metabfile)$datapath)))))
+    return(mychoices)
+  }) 
+
+  getVarToMap <- reactive({
+    templatefile=NULL
+    dir <- system.file("extdata", package="COMETS", mustWork=TRUE)
+    if(input$template == "Age") {
+          templatefile <- file.path(dir, "cometsInputAge.xlsx")
+    }
+    else if(input$template == "Basic") {
+          templatefile <- file.path(dir, "cometsInputBasic.xlsx")
+    }
+    else{return(NULL)}
+    tomap<-suppressWarnings(fixData(readxl::read_excel(templatefile, 4)))$varreference
+    return(tomap)
+  })
+
+output$OtherVarMaps <- renderUI({
+    mychoices=req(getIDChoices())
+    tomap=req(getVarToMap())
+    lapply(tomap, function(i) {
+           selectInput(i,paste0("Select ",i),c("Optional",mychoices))
+    })
+  })
 
  output$downloadExcell <- downloadHandler(
- 	filename = "MyData.xlsx",
+        filename = "MyData.xlsx",
          content = function(outputfile) {
-             myvarmap=c()
-             if(input$metabid != "Optional") {
-		myvarmap$metabid=input$metabid }
-             if(input$subjid != "Optional"){
-                myvarmap$id=input$subjid }
-             if(input$ageid != "Optional"){
-                myvarmap$age=input$ageid }
-             if(input$bmiid != "Optional"){
-                myvarmap$bmi=input$bmiid }
-          
+             mychoices=req(getIDChoices())
+             tomap=req(getVarToMap())
+print(tomap)
+             myvarmap=c() 
+             myvarmap=as.data.frame(lapply(tomap, function(x) {
+#                print(paste("Grabbing", eval(x), "which is ",input[[eval(x)]])) 
+                as.character(unlist(input[[eval(x)]]))
+                #myvarmap[[eval(x)]]=input[[eval(x)]]
+                #print(paste("myvarmap entry",myvarmap[[eval(x)]]))
+             }))
+             colnames(myvarmap)=tomap
+             print("Myvarmap before changing Optional")
+             print(myvarmap)
+	     myvarmap[which(myvarmap=="Optional")]="Needs User Input"
+
+             for (i in 1:length(myvarmap)) {
+                  myvarmap[[i]]=as.character(myvarmap[[i]])
+             }
+
+             print(paste("Dim",dim(myvarmap)))
+	     print("Printing myvarmap")
+             print(myvarmap)
+
              if(!is.null(myvarmap)) {myvarmap=as.data.frame(myvarmap)}
 
-              COMETS::createCOMETSinput(
+              COMETS::createCOMETSinput(template=tolower(req(input$template)),
                  filenames= req(loadInputFiles()), varmap=myvarmap,outputfile=outputfile)
-         })
-
- output$selectMETABid <- renderUI({
-        mychoices=c("Optional",colnames(read.csv(req(as.character(parseFilePaths(rootVolumes,input$metabfile)$datapath)))))
-        selectInput("metabid","Select Metabolite Id",mychoices)
-  })
-
- output$selectSUBJid <- renderUI({
-        mychoices=c("Optional",colnames(read.csv(req(as.character(parseFilePaths(rootVolumes,input$subjfile)$datapath)))))
-        selectInput("subjid","Select Subject Id",mychoices)
-  })
-
- output$selectAGEid <- renderUI({
-        mychoices=c("Optional",colnames(read.csv(req(as.character(parseFilePaths(rootVolumes,input$subjfile)$datapath)))))
-        selectInput("ageid","Select Age Id",mychoices)
-  })
-
- output$selectBMIid <- renderUI({
-        mychoices=c("Optional",colnames(read.csv(req(as.character(parseFilePaths(rootVolumes,input$subjfile)$datapath)))))
-        selectInput("bmiid","Select BMI Id",mychoices)
-  })
+   })
 
 })
