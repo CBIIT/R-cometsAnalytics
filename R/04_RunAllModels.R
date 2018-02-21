27c4026656e59f3d9e46cd8805a7defbf770d434
EllaTemprosa <ella@bsc.gwu.edu>
2018-02-21 11:44:18 -0500

    added HMDB to harmonization which includes new masterhmdb in compileduids.rdata added function for table 1 characteristics runDescrip added function to output list to excel file , each item corresponds to worksheet

--- R/04_RunAllModels.R
+++ R/04_RunAllModels.R
@@ -4,7 +4,7 @@
 #' @param cohort cohort label (e.g. DPP, NCI, Shanghai)
 #' @param writeTofile T/F (whether or not to write results for each model onto
 #' separate xls file). Files are written to current directory. Default is True.
-#' 
+#'
 #' @return a list of data frames, where each data frame has rows representing the correlation for each combination of outcomes and exposures with additional columns for n, pvalue, metabolite_id, method of model specification (Interactive or Batch), name of the cohort and adjustment variables. Attribute of dataframe includes ptime for processing time of model run.
 #'
 #' @examples
@@ -25,10 +25,53 @@ runAllModels <- function(readData, cohort="", writeTofile=T) {
         print(paste("Running",i))
 	mymod <- getModelData(readData,modlabel=i)
         mycorr <- runCorr(mymod,readData,cohort)
-	results[[i]] <- mycorr  
+	results[[i]] <- mycorr
         if (writeTofile) {
               OutputCSVResults(i,mycorr,cohort=cohort)
         }
   }
   return(results)
+}
+
+
+
+#' This function allows users to run all models that are provided in the "Models" sheet of
+#' the input Excell file.
+#' @param readData list from readComets
+#' @param cohort cohort label (e.g. DPP, NCI, Shanghai)
+#'
+#' @return a list with 2 data frames, continuous and categorical summaries. Type of variable is defined in varmap
+#'
+#' @examples
+#' dir <- system.file("extdata", package="COMETS", mustWork=TRUE)
+#' csvfile <- file.path(dir, "cometsInputAgeTest.xlsx")
+#' exmetabdata <- readCOMETSinput(csvfile)
+#' allmodeloutput <- runAllModels(exmetabdata)
+#' # Get descriptive data
+#' descdata <-runDescrip(exmetabdata)
+#' OutputXLSResults(filename="corr",datal=descdata,cohort="DPP")
+#'
+#' @export
+
+runDescrip<- function(readData){
+  sumcat<-NULL
+  # check if vartype is in vmap to see whether anyvars are categorical
+  if (which(grepl("vartype",names(readData$vmap)))>0){
+
+  catvars<-names(readData$subjdata)[which(sapply(readData$subjdata, is.factor)==TRUE)]
+
+  msdata<-readData$subjdata %>%
+    select_(catvars)
+  msdata <- data.table::melt(readData$subjdata,measure.vars=catvars)
+  sumcat <- msdata %>%
+    group_by(variable, value) %>%
+    summarise (n = n()) %>%
+    mutate(proportion = n / sum(n))
+  }
+
+  sumcnt <-psych::describe(readData$subjdata,quant = c(.05,.25,.5,.75,.95))
+  sumcnt$varname<-rownames(sumcnt)
+
+  return(list(sumcat=sumcat,sumall=sumcnt))
+
 }
