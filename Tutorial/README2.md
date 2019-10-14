dd3788d8855f87f9048460fa2a9191b1c84075e8
EllaTemprosa <ella@bsc.gwu.edu>
2019-10-14 10:58:51 -0400

    Initial tutorial move

---
+++ Tutorial/README2.md
@@ -0,0 +1,13 @@
+# COMETS Book
+This book includes the tutorial, vignette and manual for using comets-analytics.org. It is maintained by the comets-analytics workgroup.
+
+Some important notes for contributors:
+
+1. _imgppt subdirectory holds all powerpoint files that needs to be converted to png and needs to be saved to \_book\catBook_files\sdfigs-html
+2. The book is rendered in alphanumeric order
+3. 
+
+Quick step by step guide - terminal 
+
+
+Quick step by step guide - sourcetree 
---
+++ Tutorial/chunks/chunks.R
@@ -0,0 +1,41 @@
+## ---- subjectdata ----
+tufte::margin_note("<span class='texta'>**a.Describe Model: **</span> Name your model
+  <br>&nbsp;<br>
+            <span class='textb'>**b.Select Exposure: ** </span> i.e. your independent variable(s) of interest from the drop down list; the drop down options will include all the variables in your ‘VarMap’. The beta coefficient for this variable will be returned in the results 
+            <br>&nbsp;<br>
+            <span class='textc'>**c.Select your outcome: **</span> i.e. your dependent variable(s) of interest from the drop down list
+            <br>&nbsp;<br>
+            <span class='textd'>**d.Adjusted covariates**</span> Select any variables from the drop down that are potential confounders and should be included in the model (optional)
+            <br>&nbsp;<br>
+            <span class='texte'>**e.Strata by **</span> for stratified analyses only. Select variable, and results will be returned for all strata within this variable (optional)
+            <br>&nbsp;<br>
+            <span class='texte'>**f.Where**</span> If analysis will only be run on a sunset of the cohort as defined by this variable. A where statement can be constructed using the ‘operator’ and ‘Compare to’ (eg. Age <18) (optional)
+            <br>&nbsp;<br>
+            <span class='texte'>**g.Run Model **</span> Click to run model. Or click rest to clear the work space and start a new model
+            <br>&nbsp;<br>
+            <span class='texte'>** Timing **</span> Depending on the size of your data, the complexity of the model and the current analysis load on the server this may take a few seconds or a few minutes.
+            
+            ")
+
+
+## ---- interactive ----
+  margin_note("<span class='texta'>**a.Describe Model: **</span> Name your model
+  <br>&nbsp;<br>
+  <span class='textb'>**b.Select Exposure: ** </span> i.e. your independent variable(s) of interest from the drop down list; the drop down options will include all the variables in your ‘VarMap’. The beta coefficient for this variable will be returned in the results 
+  <br>&nbsp;<br>
+  <span class='textc'>**c.Select your outcome: **</span> i.e. your dependent variable(s) of interest from the drop down list
+  <br>&nbsp;<br>
+  <span class='textd'>**d.Adjusted covariates**</span> Select any variables from the drop down that are potential confounders and should be included in the model (optional)
+  <br>&nbsp;<br>
+  <span class='texte'>**e.Strata by **</span> for stratified analyses only. Select variable, and results will be returned for all strata within this variable (optional)
+  <br>&nbsp;<br>
+  <span class='texte'>**f.Where**</span> If analysis will only be run on a sunset of the cohort as defined by this variable. A where statement can be constructed using the ‘operator’ and ‘Compare to’ (eg. Age <18) (optional)
+   <br>&nbsp;<br>
+  <span class='texte'>**g.Run Model **</span> Click to run model. Or click rest to clear the work space and start a new model
+ <br>&nbsp;<br>
+  <span class='texte'>** Timing **</span> Depending on the size of your data, the complexity of the model and the current analysis load on the server this may take a few seconds or a few minutes.
+
+  ")
+ # print('<a href="static/output_interactivemode_2.png"><img src="static/output_interactivemode_2.png" style="width: 60%"></a>')
+
+
